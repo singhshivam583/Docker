d0ae31c6127d678db211a8ef9e260b5a5b1be51e
Shvam Singh <singhshivam583@yahoo.in>
2024-01-04 16:33:01 +0530

    update

---
+++ multi-stage/src/routes/router.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "Hello from Docker 🎉" })
+});
+
+router.get("/health", (req, res) => {
+  res.status(200).json({ message: "Everything is good here 👀" });
+});
+
+export default router ;
